ff3f712c2090f1342b2219d453f63e3e8922bff4
Arqam Mazhar <Arqam39>
2022-05-22 17:20:34 +0500

    Added code for connecting to WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,47 @@
 #include <Arduino.h>
+#include <WiFi.h>
+// #include <ESPAsyncWebServer.h>
+// #include <FS.h>
+
+const char* ssid = "mywifi";
+const char* password = "12345678";
+
+
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  // Wait for wifi to be connected
+ uint32_t notConnectedCounter = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+      notConnectedCounter++;
+      if(notConnectedCounter > 15) { // Reset board if not connected after 5s
+          Serial.println("Resetting due to Wifi not connecting...");
+          ESP.restart();
+      }
+  }
+  Serial.print("Wifi connected, IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN,HIGH);
-  Serial.println("LED is On.");
-  delay(1000);
-  digitalWrite(LED_BUILTIN,LOW);
-  Serial.println("LED is Off.");
-  delay(1000);
+    unsigned long currentMillis = millis();
+  // if WiFi is down, try reconnecting every CHECK_WIFI_TIME seconds
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >=interval)) {
+    Serial.print(millis());
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    previousMillis = currentMillis;
+  }
 }
+
