87ac499d4b0f826a0d8d9eca94ad4509a3674bc2
Arqam Mazhar <Arqam39>
2022-05-28 13:21:31 +0500

    Setting up the async web server and async web socket for server side handling of client connection, disconnection and msges transfer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
-#include <Arduino.h>
+//#include <Arduino.h>
 #include <WiFi.h>
-// #include <ESPAsyncWebServer.h>
-// #include <FS.h>
+#include <ESPAsyncWebServer.h>
+//#include <FS.h>
 
+//declaring an object of type asyncwebserver to control functions related to server defining port no 80 for server to listen to
+AsyncWebServer server(80);
+//defining the web socket route
+AsyncWebSocket ws("/chat");
+
+//initiailizing variables to hold wifi credentials
 const char* ssid = "mywifi";
 const char* password = "12345678";
 
 
+
 unsigned long previousMillis = 0;
 unsigned long interval = 30000;
+
+//function for handling the websocket clients (connection, disconnection, and forwarding received data to all the clients connected)
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len){
+  if(type == WS_EVT_CONNECT){
+    Serial.println("Websocket client Connection Established");
+  }
+  else if(type == WS_EVT_DISCONNECT){
+    Serial.println("Client Disconnected");
+  }
+  else if(type == WS_EVT_DATA){
+    ws.textAll(data, len);
+    Serial.println("Dtata Received: ");
+    for(int i=0; i<len; i++)
+      Serial.print((char) data[i]);
+  }
+  Serial.println();
+}
 
 
 void setup() {
@@ -30,6 +54,14 @@ void setup() {
   }
   Serial.print("Wifi connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+
+//after connecting to wifi 
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+
+  server.begin();
+
 }
 
 
